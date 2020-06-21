ca1323f2cd812053eb80c6aa41966f4ce50ef472
csae8092 <peter.andorfer@oeaw.ac.at>
2020-06-21 18:02:45 +0200

    added first tests

--- tests/test_pylobid.py
+++ tests/test_pylobid.py
@@ -5,7 +5,7 @@
 
 import unittest
 
-from pylobid import pylobid
+from pylobid import pylobid as pl
 
 
 class TestPylobid(unittest.TestCase):
@@ -17,5 +17,13 @@ class TestPylobid(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_000_ID_PATTERN(self):
+        pl_client = pl.PyLobidClient()
+        GND_IDS = pl_client.TEST_IDS
+        self.assertEqual(type(GND_IDS), list, "should be a list")
+
+    def test_001_get_entity_lobid_url(self):
+        pl_client = pl.PyLobidClient()
+        for x in pl_client.TEST_IDS:
+            lobid_url = pl_client.get_entity_lobid_url(x)
+            self.assertEqual(lobid_url[0], 'h', "should be 'h'")
