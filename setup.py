2e429c69a77bbefdaf028621749ee16a4d57f8e7
csae8092 <peter.andorfer@oeaw.ac.at>
2021-03-07 17:43:14 +0100

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/csae8092/pylobid',
-    version='1.2.0',
+    version='1.3.0',
     zip_safe=False,
 )
